c4167ef3f9b81dfed142ed70d3c0a75fb8a69b41
Juansito_the_dev <tecnosebastube@gmail.com>
2021-01-03 20:41:50 -0500

    arreglando detalles con las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import Layout from './components/Layout';
 import Move from './components/Move';
 import Form from './containers/Form';
@@ -10,7 +10,7 @@ const App = () => {
         <BrowserRouter>
             <Layout />
             <Switch>
-                <Route exact path='/' component={Move} />
+                <Route exact path='/text/build' component={Move} />
                 <Route exact path='/text' component={Text} />
                 <Route exact path='/text/form' component={Form} />
             </Switch>
